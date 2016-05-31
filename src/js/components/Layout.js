4267fddf1a188f991ccbb8252119e15626f28d28
eoff <bane.dev101@gmail.com>
2016-05-31 10:45:50 +0200

    Add top padding

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default class Layout extends React.Component {
   render() {
     return (
-      <h1>It works!</h1>
+      <main className="container">
+      </main>
     );
   }
 }
