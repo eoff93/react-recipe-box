a949339be271776541bff1073e20da71a3f16b1b
eoff <bane.dev101@gmail.com>
2016-05-31 11:10:27 +0200

    Fix capitalization of React.Component

---
+++ src/js/components/IngredientList/Ingredient.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Panel } from 'react-bootstrap';
+
+export default class Ingredient extends React.Component {
+  render() {
+    return (
+      <Panel>
+        Lorem ipsum dolor sit amet
+      </Panel>
+    );
+  }
+}
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Ingredient from './IngredientList/Ingredient';
 
 export default class Layout extends React.Component {
   render() {
     return (
       <main className="container">
+        <Ingredient />
       </main>
     );
   }
