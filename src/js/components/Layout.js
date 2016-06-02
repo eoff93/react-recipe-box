6c32496a2173c246003423042b96a5f0faec2851
eoff <bane.dev101@gmail.com>
2016-06-02 14:09:21 +0200

    Add mockup state

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,6 +3,15 @@ import RecipeBox from './RecipeBox';
 import AddRecipe from './AddRecipe';
 
 export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        { title: 'stuff', ingredients: ['stuff1', 'stuff2', 'stuff3'] },
+        { title: 'things', ingredients: ['things1', 'things2', 'things3'] },
+      ],
+    };
+  }
   render() {
     return (
       <main className="container">
